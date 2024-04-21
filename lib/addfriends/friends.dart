af983d631787dce54db59b496cba6ccbe603f773
Bharath Vishnu B <bharathvb1204@gmail.com>
2024-04-21 09:29:55 +0530

    add friends and your friends

--- lib/addfriends/friends.dart
+++ lib/addfriends/friends.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class AddPage extends StatelessWidget {
+class AddPage extends StatefulWidget {
+  @override
+  _AddPageState createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+  List<Map<String, String>> yourFriends = [];
+  List<Map<String, String>> addFriends = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    setState(() {
+      addFriends = _generateDummyData();
+    });
+  }
+
+  void _moveToAddFriends(int index) {
+    setState(() {
+      Map<String, String> friend = addFriends.removeAt(index);
+      yourFriends.add(friend);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Colors.black.withOpacity(.5),
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
@@ -13,10 +40,6 @@ class AddPage extends StatelessWidget {
             centerTitle: true,
             automaticallyImplyLeading: false,
             backgroundColor: Colors.white,
-            // leading: IconButton(
-            //   onPressed: () => Navigator.pop(context),
-            //   icon: Icon(FontAwesomeIcons.arrowDown),
-            // ),
             actions: [
               IconButton(
                 onPressed: () => Navigator.pop(context),
@@ -66,86 +89,110 @@ class AddPage extends StatelessWidget {
             child: Container(
               padding: EdgeInsets.all(12.0),
               child: Text(
-                "Quick Add",
+                "Your Friends",
                 style: TextStyle(fontWeight: FontWeight.w800),
               ),
             ),
           ),
-          FutureBuilder(
-            future: getAddFriendList(),
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.hasData) {
-                final List<Map<String, String>> dummyData = snapshot.data;
-                return SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: ListTile(
-                          leading: CircleAvatar(
-                            radius: 25.0,
-                            backgroundColor: Colors.black.withOpacity(.5),
-                          ),
-                          title: Text(
-                            dummyData[index]['username']!,
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 16.0,
-                              color: Colors.black,
-                            ),
-                          ),
-                          subtitle: Text(dummyData[index]['userID']!),
-                          trailing: Container(
-                            height: 35.0,
-                            width: 80.0,
-                            padding: EdgeInsets.all(5.0),
-                            decoration: BoxDecoration(
-                              color: Colors.black.withOpacity(.80),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(30.0)),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Icon(
-                                  Icons.add,
-                                  size: 16.0,
-                                  color: Colors.white,
-                                ),
-                                Text(
-                                  "Add",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 12.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      leading: CircleAvatar(
+                        radius: 25.0,
+                        backgroundColor: Colors.black.withOpacity(.5),
+                      ),
+                      title: Text(
+                        yourFriends[index]['username']!,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16.0,
+                          color: Colors.black,
                         ),
-                      );
-                    },
-                    childCount: dummyData.length,
+                      ),
+                      subtitle: Text(yourFriends[index]['userID']!),
+                    ),
                   ),
                 );
-              } else {
-                return SliverFillRemaining(
-                  child: Center(
-                    child: CircularProgressIndicator(),
+              },
+              childCount: yourFriends.length,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              padding: EdgeInsets.all(12.0),
+              child: Text(
+                "Add Friends",
+                style: TextStyle(fontWeight: FontWeight.w800),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 25.0,
+                      backgroundColor: Colors.black.withOpacity(.5),
+                    ),
+                    title: Text(
+                      addFriends[index]['username']!,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                    subtitle: Text(addFriends[index]['userID']!),
+                    trailing: InkWell(
+                      onTap: () => _moveToAddFriends(index),
+                      child: Container(
+                        height: 35.0,
+                        width: 80.0,
+                        padding: EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(.80),
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Icon(
+                              Icons.add,
+                              size: 16.0,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              "Add",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 );
-              }
-            },
+              },
+              childCount: addFriends.length,
+            ),
           ),
         ],
       ),
     );
-  }
-
-  Future<List<Map<String, String>>> getAddFriendList() async {
-    await Future.delayed(Duration(seconds: 2));
-    return _generateDummyData();
   }
 
   List<Map<String, String>> _generateDummyData() {
@@ -155,6 +202,11 @@ class AddPage extends StatelessWidget {
       {"username": "Billabong", "userID": "bvb09"},
       {"username": "javad", "userID": "jaaa"},
       {"username": "jayadev", "userID": " JD"},
+      {"username": "Alice", "userID": "alice123"},
+      {"username": "Bob", "userID": "bob456"},
+      {"username": "Charlie", "userID": "charlie789"},
+      {"username": "David", "userID": "david1011"},
+      {"username": "Eve", "userID": "eve1213"},
     ];
   }
 }
